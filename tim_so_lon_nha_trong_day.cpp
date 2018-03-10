f7bad2176accfd41fad63b48fc050da32ba3a036
jimmy706 <34393773+jimmy706@users.noreply.github.com>
2018-03-11 00:53:40 +0700

    Update tim_so_lon_nha_trong_day.cpp

--- tim_so_lon_nha_trong_day.cpp
+++ tim_so_lon_nha_trong_day.cpp
@@ -3,25 +3,33 @@ nguồn code dùng cho lập trình C căn bản
 
 #include <stdio.h>
 #include <conio.h>
+#include <math.h>
 
-
-// Tim so lon nhat trong day
+// Tim so be nhat, lon nhat trong day
 
 int main(){
-	unsigned n,sothu,vitri;
-	int a,max=0;
-	printf("nhap vao so day: ");
-	scanf("%d", &n);
+	int n,sothu,a[1000],max=0,min,vitri_max,vitri_min;
+	printf("Nhap vao N so nguyen: ");
+	scanf("%d",&n);
 	for(sothu = 1; sothu <= n; sothu++){
-		printf("\nNhap vao so thu %d: ", sothu);
-		scanf("%d", &a);
-		
-		if(a>max){
-			max = a;
-			vitri = sothu;
+		printf("\nNhap vao so thu %d: ",sothu);
+		scanf("%d", &a[sothu]);
+	}
+	for(sothu=1;sothu<=n;sothu++){
+		if(a[sothu] > max){
+			max = a[sothu];
+			vitri_max = sothu;
 		}
 	}
-	
-	printf("So lon nhat trong day la: %d o vi tri thu %d", max, vitri);
+	printf("\n\nGia tri lon nhat trong day so la %d ; o vi tri thu %d",max,vitri_max);
+	min = a[0];
+	for(sothu=1;sothu<=n;sothu++){
+		if(min>a[sothu]){
+			min = a[sothu];
+			vitri_min = sothu;
+		}	
+	}
+	printf("\n\nGia tri be nhat trong day la %d ; o vi tri thu %d", min,vitri_min);
 	return 0;
+	
 }

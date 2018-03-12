e13b51df5688369b8369882e51d98eb291556c9d
jimmy706 <34393773+jimmy706@users.noreply.github.com>
2018-03-12 21:52:37 +0700

    Update B1709272.cpp

--- B1709272.cpp
+++ B1709272.cpp
@@ -1,10 +1,3 @@
-/* Ho ten: Dang Quoc Dung
-   Lop: 17Y9A1
-   Ma SV: B1709272
-   May so: 35
-   De: 01
-   	*/
-
 // Chuong trinh nhap vao N dien tich HCN, in ra chieu dai va chieu rong cua HCN co S lon nhat
 
 #include <stdio.h>
@@ -15,31 +8,46 @@
 
 int main(){
 	unsigned int n,dem;
-	float rong,dai,S,max=0,dai_max,rong_max;
+	float rong[1000],dai[1000],S[1000],max=0,dai_max,rong_max,dai_min,rong_min,min;
 	printf("Nhap vao so hinh chu nhat muon tinh dien tich: ");
 	scanf("%d",&n);
 	
 	for(dem=1;dem<=n;dem++){
 		printf("\nChieu dai S%d: ", dem);
-		scanf("%f",&dai);
+		scanf("%f",&dai[dem]);
 		printf("\nChieu rong S%d: ",dem);
-		scanf("%f",&rong);
-		if(dai<0 || rong <0)
+		scanf("%f",&rong[dem]);
+		if(dai[dem]<0 || rong[dem] <0)
 			printf("\nBan da nhap sai gia tri ,xin hay xem lai");
 		else{
 		
-		S = dai*rong;
-		printf("\nDien tich hinh chu nhat thu %d: %.3f",dem,S);	
-			if(S>max){
-				max=S;
-				dai_max = dai;
-				rong_max=rong;
+		S[dem ]= dai[dem]*rong[dem];
+		printf("\nDien tich hinh chu nhat thu %d: %.3f",dem,S[dem]);	
+			if(S[dem]>max){
+				max=S[dem];
+				dai_max = dai[dem];
+				rong_max=rong[dem];
 				}
 			}
 		
 	}
+	dai_min = dai[1];
+	rong_min = rong[1];
+	min = S[1];
+	for(dem=1; dem<=n;dem++){
+		if(	min>S[dem]){
+			min = S[dem];
+			dai_min = dai[dem];
+			rong_min = rong[dem];
+		}
+	}
+	
+	
 	printf("\n\n Dien tich HCN lon nhat la: %.3f",max );
-	printf("\n\n Chieu dai va chieu rong lon nhat cua HCN lon nhat la: %.2f va %.2f", dai_max,rong_max);
+	printf("\n\n Chieu dai va chieu rong cua HCN lon nhat la: %.2f va %.2f", dai_max,rong_max);
+	
+	printf("\n\n Dien tich HCN nho nhat la: %.3f", min);
+	printf("\n\n Chieu dai va rong cua HCN be nhat la: %.2f va %.2f", dai_min,rong_min);
 	getch();
 	return 0;
 }
